11d09f62310b0b627a989587b184c7207e40dbdd
Emil Nuutinen <emil@emilnuutinen.com>
2021-08-18 16:08:22 +0300

    add RSS feed

---
+++ scripts/gen_rss.js
@@ -0,0 +1,35 @@
+const { promises: fs } = require('fs')
+const path = require('path')
+const RSS = require('rss')
+const matter = require('gray-matter')
+
+async function generate () {
+  const feed = new RSS({
+    title: 'Emil Nuutinen',
+    site_url: 'https://emilnuutinen.com',
+    feed_url: 'https://emilnuutinen.com/feed.xml'
+  })
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'posts'))
+
+  await Promise.all(
+    posts.map(async (name) => {
+      if (name.startsWith('index.')) return
+
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'posts', name)
+      )
+      const frontmatter = matter(content)
+
+      feed.item({
+        title: frontmatter.data.title,
+        url: '/posts/' + name.replace(/\.md?/, ''),
+        date: frontmatter.data.date
+      })
+    })
+  )
+
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+}
+
+generate()
