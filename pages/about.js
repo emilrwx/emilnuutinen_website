74858e9eac845548c3571aecca434a88002ccb9e
Emil Nuutinen <emil@emilnuutinen.com>
2021-08-28 12:49:10 +0300

    Update content

--- pages/about.js
+++ pages/about.js
@@ -12,17 +12,18 @@ export default function About () {
         <h2>Short version</h2>
         <p><b>Name:</b> Emil Nuutinen <br />
           <b>Location:</b> Turku, Finland <br />
-          <b>Education:</b> B.B.A. Business Information Technology, Turku University of Applied Sciences <br />
-          <b>Interests:</b> Data Science, Software Development, Linux, FOSS, Privacy, Reading, Philosophy, Psychology, Gym, Gaming, Introverting, Over-engineered note-taking methods & Drinking too much coffee <br />
+          <b>Education:</b> BBA Business Information Technology, Turku University of Applied Sciences (2017-2020)<br />
+          <b>Education:</b> MSc Computer Science, University of Turku (2021-2023)<br />
+          <b>Interests:</b> NLP, Machine Learning, Software Development, Linux, FOSS, Privacy, Reading, Philosophy, Psychology, Gym, Gaming, Introverting, Over-engineered note-taking methods & Drinking too much coffee <br />
           <b>Contact:</b> hello@emilnuutinen.com
         </p>
 
         <h2>Long version</h2>
         <p>Hi, my name is Emil. I'm a <b>software developer</b> studying to become a <b>data scientist</b>.</p>
 
-        <p> I graduated with a degree in <b>Business Information Technology</b> in December 2020 and I'm going to start my C.S. Master's in September 2021. My goal is to work in research some day.</p>
+        <p> I graduated with a degree in <b>Business Information Technology</b> in December 2020 and I'm now doing <b>Computer Science</b> masters. My goal is to work in research some day.</p>
 
-        <p>Professionally I'm interested in <b>data science</b> generally and in privacy respecting software solutions. I'm concerned about the current state of software and privacy. I believe we can build innovative data-oriented products without violating the privacy of the users and I want to contribute into building them.</p>
+        <p>Professionally I'm interested in <b>natural language processing</b>, <b>machine learning</b> and <b>privacy respecting software solutions</b>. I'm concerned about the current state of software and privacy. I believe we can build innovative data-oriented products without violating the privacy of the users and I want to contribute into building them.</p>
 
         <p>I'm also interested in <b>robotics</b> and <b>self-driving cars</b>. I would love to see a future where humans don't drive anymore and robots handle all the task that are repetitive, dangerous or need precision that we humans are not capable of.</p>
 
